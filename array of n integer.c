82fdf6d9f47a2ee665f4f97cddf092764d83d854
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-01-21 11:02:14 +0530

    Create array of n integer.c

---
+++ array of n integer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,k,i;
+sum=0;
+scanf("%d%d",&n&k);
+printf("%d%d",n,k);
+for(i=0;i<ni++)
+printf("%d",a[i]);
+for(i=1;i<=k;i++)
+sum=suma[i];
+printf("5d",sum);
+}
