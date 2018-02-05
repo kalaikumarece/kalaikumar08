6f81dce5414501fee2c8cf41922f5b4ef7da68ab
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-02-05 15:58:56 +0530

    Create special string.c

---
+++ special string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char ar[10]="******";
+    char ar2[10]="######";
+    char ar3[10]="!!!!!!!!";
+    printf("%s\n  %s\n  %s\n ",ar,ar2,ar3);
+
+  return 0;
+}
