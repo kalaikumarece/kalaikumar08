76fd6974b5b270bdb2e8de706365e627287a9c5d
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-02-07 16:27:51 +0530

    Create number in range or not.c

---
+++ number in range or not.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{int n;
+    printf("enter the no");
+    scanf("%d",n);
+    if(n<=1&&n>=2)
+    printf("%d is in range",n);
+    else
+    printf("not in range");
+
+
+    return 0;
+}
