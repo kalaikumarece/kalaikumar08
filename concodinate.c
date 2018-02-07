bdddaf84f24bdc1321510b4a58cef2648ea8c6ad
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-02-07 16:09:28 +0530

    Create concodinate.c

---
+++ concodinate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+char a[10],b[10];
+char str1,str2;
+printf("enter the string1");
+scanf("%s",a);
+printf("enter the str2");
+scanf("%s",b);
+printf("concadinated str is %s%s",a,b);
+
+    return 0;
+}
