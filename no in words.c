e582a5e987e538783918337e321b1f72cd91a219
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-02-12 16:34:10 +0530

    Create no in words.c

---
+++ no in words.c
@@ -0,0 +1,38 @@
+int main()
+{
+    int i,a,n;
+    printf("enter the element");
+    scanf("%d",&n);
+    printf("enter the no of digits");
+    scanf("%d",&a);
+    for(i=0;i<a;i++)
+    {
+    switch(n)
+    {
+        case 0:
+        printf("zero");
+        case 1:
+        printf("one");
+        case 2:
+        printf("two");
+        case 3:
+        printf("three");
+        case 4:
+        printf("four");
+        case 5:
+        printf("five");
+        case 6:
+        printf("six");
+        case 7:
+        printf("seven");
+        case 8:
+        printf("eight");
+        case 9:
+        printf("nine");
+        break;
+    }}
+        
+    
+
+    return 0;
+}
