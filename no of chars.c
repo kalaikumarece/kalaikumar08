13a97c7928ac230b1e6a7034b96d5071598c3c35
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-02-28 15:58:26 +0530

    Rename no of char.c to no of chars.c

---
+++ no of chars.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n,count=0,countword=0;
+    char a[10];
+    printf("enter the word");
+    scanf("%s",&a[i]);
+    for(i=0;i<a[i];i++)
+    {
+        
+        if(a[i]=="");
+        countword++;
+        
+    }printf("\n no of word is %d",countword);
+    
+    return 0;
+}
