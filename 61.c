442a17a2d5e2d652c33595eb5b55f811381b9579
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-02-28 16:34:43 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int n,i;
+    char a[10];
+    printf("enter the word\n");
+    scanf("%c",&a[i]);
+printf("how many letters u want to print");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+ printf("%c",a[i]);
+ }
+    return 0;
+}
