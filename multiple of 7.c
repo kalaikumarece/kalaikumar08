cb607090ccf8f05f564a6051b27125b21aad40bb
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-03-02 15:50:28 +0530

    Create multiple of 7.c

---
+++ multiple of 7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the no");
+    scanf("%d",&a);
+    if(a%7==0)
+    printf("%d is multiple of 7",a);
+    else
+    printf("not multiple of 7");
+    return 0;
+}
