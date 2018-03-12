3836b440dad1fa82555e2dcfc6519515bbc7120d
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-03-12 16:56:05 +0530

    Create find odd digits in given num.c

---
+++ find odd digits in given num.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{int a,b,n,num,rem;
+printf("enter the number");
+scanf("%d",&num);
+while(num>n)
+{a=num%10;
+num=num/10;
+rem=a%2;
+if(rem!=0)
+printf("%d",a);
+   
+}return 0;
+}
