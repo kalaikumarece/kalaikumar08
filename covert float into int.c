6e72ed7ffb71eeef1c2c3612dd21c3c8915f9022
kalaikumarece <35127928+kalaikumarece@users.noreply.github.com>
2018-03-19 16:47:15 +0530

    Create covert float into int.c

---
+++ covert float into int.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{ 
+    float a;
+    int b;
+printf("enter the decimal point");
+scanf("%f",&a);
+b=++a;
+printf("integerre no is %d",b);
+    
+    return 0;
+    
+}
+    
